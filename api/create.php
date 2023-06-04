9f47e4c4f20ae8fcdb021ee2c640a05998c1d642
nika sanikidze <sdd990671@gmail.com>
2023-06-04 15:24:07 +0400

    added full create function

--- api/create.php
+++ api/create.php
@@ -18,18 +18,54 @@ $item->cv = $_GET['cv'];
 $item->job = $_GET['job'];
 $item->user_image = $_GET['user_image'];
 
-if (empty($item->name) || empty($item->email) || empty($item->cv) || empty($item->job) || empty($item->user_image)) {
+if (empty($item->name)) {
 
     $response = array(
         "code" => 400,
-        "message" => "Incomplete data. All fields are required."
+        "message" => "name parameter is empty or bad format"
+    );
+    echo json_encode($response);
+} elseif (empty($item->email)) {
+
+    $response = array(
+        "code" => 400,
+        "message" => "email parameter is empty or bad format"
+    );
+    echo json_encode($response);
+} elseif (empty($item->cv)) {
+
+    $response = array(
+        "code" => 400,
+        "message" => "cv parameter is empty or bad format"
+    );
+    echo json_encode($response);
+} elseif (empty($item->job)) {
+
+    $response = array(
+        "code" => 400,
+        "message" => "job parameter is empty or bad format"
+    );
+    echo json_encode($response);
+} elseif (empty($item->user_image)) {
+
+    $response = array(
+        "code" => 400,
+        "message" => "user_iamge parameter is empty or bad format"
     );
     echo json_encode($response);
 } else {
     if ($item->createUser()) {
-        // No output
-        http_response_code(204); // No Content
+        $response = array(
+            "code" => 200,
+            "message" => "user created successfully"
+        );
+        echo json_encode($response);
     } else {
-        http_response_code(500); // Internal Server Error
+
+        $response = array(
+            "code" => 400,
+            "message" => "user could not be created"
+        );
+        echo json_encode($response);
     }
 }
